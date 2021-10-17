e07763c162c70de7c3e1d4d2941c6aa388aa9c6c
hdogrul <hdogrul@MacBook-Pro.local>
2021-10-16 21:04:12 -0400

    setting up for jwt

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,22 +3,6 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-}
-
-type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-}
-
 type User {
     _id: ID
     username: String
@@ -27,12 +11,50 @@ type User {
     thoughts: [Thought]
     friends: [User]
   }
-  
-type Query {
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought}
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
   `;
 
 // export the typeDefs
